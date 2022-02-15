ed19d44d043996b2de3cb63d4c05e04d9e05e0b1
Matt Zenger <mattzenger@tamu.edu>
2022-02-15 11:53:18 -0600

    I made changes to the sum equation and then the string that were read for each variable. Now it should show a second output after the initial one.

--- python-project.py
+++ python-project.py
@@ -4,3 +4,14 @@ file = open("http_access_log.txt","r")
 data = file.read()
 total_occurences = data.count("get")
 print("Number of total requests in the log:", total_occurences)
+
+october= data.count("Oct/1994")
+november= data.count("Nov/1994")
+december= data.count("Dec/1994")
+january= data.count("Jan/1995")
+february= data.count("Feb/1995")
+march= data.count("Mar/1995")
+
+sixmonth= (october + november + december + january + february + march)
+
+print("Number of requests during the first 6 months:", sixmonth)
